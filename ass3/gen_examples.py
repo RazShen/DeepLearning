e3caf110bad09c66d0791019e05f357d0041e2a3
Raz Shenkman <shenkmanraz@gmail.com>
2018-12-04 23:06:09 +0200

    part 1

--- ass3/gen_examples.py
+++ ass3/gen_examples.py
@@ -1,4 +1,5 @@
 import rstr
+from random import shuffle
 from itertools import izip
 NUM_EXAMPLES = 500
 
@@ -13,13 +14,24 @@ def gen_specific_examples(file_name, pos=True):
 def gen_test_and_train(pos_examples, neg_examples):
     positive_examples = open(pos_examples, "r").readlines()
     negative_examples = open(neg_examples, "r").readlines()
+    all_examples = []
+    for pos_example, neg_example in izip(positive_examples, negative_examples):
+        all_examples.append((pos_example.strip("\n"), 1))
+        all_examples.append((neg_example.strip("\n"), 0))
+    shuffle(all_examples)
+    train_list = all_examples[:int(float(len(all_examples) * 0.8))]
+    test_list = all_examples[int(float(len(all_examples) * 0.8)):]
     with open("train", "w") as train:
-        for pos_example, neg_example in izip(positive_examples, negative_examples):
-            
-
+        for train_example_tuple in train_list:
+            train.write(str(train_example_tuple[0]) + " " + str(train_example_tuple[1]) + "\n")
+    with open("test", "w") as test:
+        for test_example_tuple in test_list:
+            test.write(str(test_example_tuple[0]) + " " + str(test_example_tuple[1]) + "\n")
 
 
 def gen_all_examples():
     gen_specific_examples("pos_examples")
     gen_specific_examples("neg_examples", pos=False)
 
+gen_all_examples()
+gen_test_and_train("pos_examples", "neg_examples")
